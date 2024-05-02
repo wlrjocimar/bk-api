48b9997abe441a81bb9743c27e622ac44ea0c45a
jocimar walter <jocimarwalter@bb.com.br>
2024-05-02 20:14:41 -0300

    Remover node_modules do controle de versão

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import authRoute from "./routes/auth.js"
+
+
+const app = express();
+dotenv.config()
+
+
+const connect  = async ()=>{
+  try {
+    await mongoose.connect(process.env.MONGO)
+    console.log("connected to mongoDB.")
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+mongoose.connection.on("disconnected",()=>{
+  console.log("mongoDB disconnected")
+})
+
+
+
+//midleware
+
+app.use("/auth",authRoute)
+
+
+
+
+
+
+
+app.listen(8800,()=>{
+    connect();
+    console.log("Conected to backend");
+})
